dbbc4f8038f6cd93fe6d23a2b0d6aad48dc6d5a7
naquid <manne.cederborg@yahoo.se>
2015-05-20 11:28:46 +0200

    Fixed Items

---
+++ UnityProject/Assets/Scripts/Inventory/Items/ItemMetalPipe.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ItemMetalPipe : Item {
+
+
+
+
+	override public void OnInspect()
+	{
+
+
+	}
+
+
+	override public void OnDragOver( Vector3 deltaMousePosition )
+	{
+
+	}
+
+	override public void OnAction( ) 
+	{
+
+		if (GWorld.isInvEnabled == true) 
+		{
+			if (Inventory.myInv.AddItem (itemData, transform.position)) 
+			{
+				GWorld.MarkEventDone (eventID);
+				Destroy (gameObject);
+			} 
+		} 
+		else 
+		{
+			Debug.Log ( "Need inventory to pickup item");
+		}
+
+	}
+
+}
