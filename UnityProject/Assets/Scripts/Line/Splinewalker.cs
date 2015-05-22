82f4c0228f9bacf635f096c76aca79b2f80e47ba
Naquid <manne.cederborg@yahoo.se>
2015-05-22 13:48:50 +0200

    spline stuff

--- UnityProject/Assets/Scripts/Line/Splinewalker.cs
+++ UnityProject/Assets/Scripts/Line/Splinewalker.cs
@@ -1,30 +1,62 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+public enum SplineWalkerMode
+{
+	Once,
+	Loop,
+	PingPong
+}
+
 public class Splinewalker : MonoBehaviour {
 
 	public BezierSpline spline;
 
 	public float duration;
-
 	private float progress;
 
 	public bool lookForward;
 
+	public bool goingForward = true;
+	public SplineWalkerMode mode;
+
 	private void Update ()
 	{
-		progress += Time.deltaTime / duration;
-		if (progress > 1.0f)
-		{
-			progress = 0.0f;
+		if (goingForward) {
+			progress += Time.deltaTime / duration;
+			if (progress > 1.0f) 
+			{
+				if (mode == SplineWalkerMode.Once) 
+				{
+					progress = 1.0f;
+				} 
+				else if (mode == SplineWalkerMode.Loop) 
+				{
+					progress -= 1.0f;
+				} 
+				else 
+				{
+					progress = 2.0f - progress;
+					goingForward = false;
+				}
+			}
 		}
-		transform.localPosition = spline.GetPoint (progress);
+		else 
+		{
+			progress -= Time.deltaTime / duration;
+			if (progress < 0.0f) 
+			{
+				progress = -progress;
+				goingForward = true;
+			}
+		}
+
 		Vector3 position = spline.GetPoint (progress);
 		transform.localPosition = position;
-		if (lookForward) 
-		{
-			transform.LookAt(position + spline.GetDirection(progress));
+		if (lookForward) {
+			transform.LookAt (position + spline.GetDirection (progress));
 		}
 	}
+
 
 }
