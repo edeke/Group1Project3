264b767459fa07e3c93519cccc19fe95f142e4d9
unknown <william.wennergren@WH932249.CM.lan>
2015-05-26 12:31:15 +0200

    Added new script for including invisible objects when building navmesh.

---
+++ UnityProject/Assets/Editor/NavMeshFixer.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class NavMeshFixer : ScriptableObject
+{
+	[MenuItem("Paladin/Fix Navigation Mesh")]
+	public static void FixNavMesh()
+	{        
+		Undo.RegisterSceneUndo("BakeNavMesh");
+		
+		List<Renderer> disabledObjects = new List<Renderer>();
+		
+		foreach (Renderer item in Object.FindObjectsOfType(typeof(Renderer)))
+		{
+
+			if (GameObjectUtility.AreStaticEditorFlagsSet(item.gameObject, StaticEditorFlags.NavigationStatic) && !item.enabled)
+			{
+				disabledObjects.Add(item);
+				item.GetComponent<Renderer>().enabled = true; 
+			}            
+		}
+		
+		NavMeshBuilder.BuildNavMesh(); 
+		
+		disabledObjects.ForEach( obj => obj.enabled = false );
+		
+		Debug.Log(string.Format("Done building navmesh, {0} objects affected.", disabledObjects.Count));
+	}
+}
