28631499846ca604e068c9b36730213bbd99ad62
unknown <william.wennergren@WH932249.CM.lan>
2015-06-09 14:16:38 +0200

    Changed night scene of Waterfall.

---
+++ UnityProject/Assets/Scripts/OnByNight.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OnByNight : MonoBehaviour {
+
+	void Awake(){
+
+		if (GWorld.GetTimeOfTheDay() == TimeOfDay.Night || GWorld.GetTimeOfTheDay() == TimeOfDay.Dawn) {
+			gameObject.SetActive (true);
+		} else {
+			gameObject.SetActive (false);
+		}
+	}
+
+}
