e3078d1b024b1ddbad626cbadd3039d97121fd38
unknown <william.wennergren@WH932249.CM.lan>
2015-06-10 13:17:15 +0200

    Added animations for frog and fixed occlusion bug.

---
+++ UnityProject/Assets/Scripts/Actor/Waterfall/SurferDude.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SurferDude : ClickOnActorBase {
+
+	private bool isTalking;
+	private bool isWalking;
+
+	Animator anim;
+
+	Vector3 posPrev;
+	float maxSpeed = 8;
+
+	// Use this for initialization
+	void Start () {
+
+		anim = GetComponentInChildren<Animator> ();
+	
+	}
+
+	void Update (){
+		
+		//calculate speed
+		float speed = (transform.position - posPrev).magnitude / Time.deltaTime / maxSpeed;
+		//speed /= maxSpeed;
+		anim.SetFloat ("speed", speed);
+		
+		posPrev = transform.position;
+		
+		if (isTalking == true) {
+			anim.SetBool ("Talk", true);
+		} 
+		
+		if (isTalking == true && GWorld.dialogOpen == false) {
+			anim.SetBool ("Talk", false);
+			isTalking = false;
+			
+		}
+		
+	}
+	
+	override public void OnTalkTo()
+	{
+		
+		Dialoguer.StartDialogue (dialog, null);
+		
+		isTalking = true;
+		
+	}
+	
+	override public void OnInspect()
+	{	
+		//DisplaySpeechBubble ( "Hello" );
+		DisplaySpeechBubble ( "Hey duuude!" );
+		
+	}
+
+	override public EAnimationState AnimationOnItem(EItem itemType)
+	{
+		switch (itemType) 
+		{
+					
+		default :
+			return EAnimationState.Error;
+		}
+	}
+}
