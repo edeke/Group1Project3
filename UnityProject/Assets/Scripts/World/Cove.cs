71a18a22f54e1c0273ec95253da8eb50f401bbbf
Naquid <manne.cederborg@yahoo.se>
2015-06-11 16:29:27 +0200

    Connected areas

---
+++ UnityProject/Assets/Scripts/World/Cove.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Cove : MonoBehaviour {
+	
+	public GameObject spawnFromWaterfall;
+	public GameObject spawnFromCave;
+
+	void Awake()
+	{
+
+		switch (GWorld.currentZone) 
+		{
+			case ZoneBase.Cove :
+				//GWorld.SpawnPlayer(  spawnFromTest2.transform  );
+			break;
+
+			case ZoneBase.WaterFall :
+				GWorld.SpawnPlayer(  spawnFromWaterfall.transform  );
+			break;
+
+			case ZoneBase.Cave :
+				GWorld.SpawnPlayer(  spawnFromCave.transform  );
+			break;
+
+			default :
+				GWorld.SpawnPlayer(  spawnFromCave.transform  );
+			break;
+
+		}
+
+		GWorld.currentZone = ZoneBase.Cove;
+
+	}
+
+
+}
