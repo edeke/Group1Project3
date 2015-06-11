00c670112a5ac184a900af4094509941f59a65cf
unknown <nor.aneer@WH93151E.CM.lan>
2015-06-11 12:14:26 +0200

    Polish on Beach scene

---
+++ UnityProject/Assets/Scripts/Actor/Beach/CampFire.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CampFire : CommentActorBase {
+
+	override public void OnInspect()
+	{		
+		//DisplaySpeechBubble ("That's not mine!");
+		DisplayComment ("A Campfire.");
+	}
+
+	override public void OnAction()
+	{
+		DisplayComment ("I don't want to touch that.");
+	}
+
+	/*override public void OnTalkTo()
+	{
+
+	}*/
+
+}
