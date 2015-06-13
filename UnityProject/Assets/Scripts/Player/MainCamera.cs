b173d8f2edb2937a5acd74953e7f946b41b8bf31
naquid <manne.cederborg@yahoo.se>
2015-06-13 15:22:06 +0200

    Playing around with outline camera

---
+++ UnityProject/Assets/Scripts/Player/MainCamera.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainCamera : MonoBehaviour {
+
+	public OutlineCamera outlineCamera;
+
+
+	// Use this for initialization	
+	void OnPostRender()
+	{
+		outlineCamera.RenderOutlines ();
+	}
+
+}
---
+++ UnityProject/Assets/Scripts/Player/OutlineCamera.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OutlineCamera : MonoBehaviour {
+	
+	RenderTexture currentRT;
+	public Camera cam;
+
+	void OnGUI()
+	{
+		Rect newRect = new Rect (0, 0, Screen.width, Screen.height);
+		GUI.DrawTexture ( newRect, currentRT);
+	}
+
+	public void RenderOutlines()
+	{
+		if (currentRT == null || currentRT.width != Screen.width || currentRT.height != Screen.height) 
+		{
+			currentRT = new RenderTexture(Screen.width, Screen.height, 16, RenderTextureFormat.ARGB32);
+		}
+
+		RenderTexture prevRT = RenderTexture.active;
+		RenderTexture.active = currentRT;
+		cam.targetTexture = currentRT;
+
+		cam.Render();
+		RenderTexture.active = prevRT;
+
+	}
+}
