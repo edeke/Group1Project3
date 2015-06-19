ddcd33aebd684543314de8c719081a80e858aa37
Naquid <manne.cederborg@yahoo.se>
2015-06-19 15:24:02 +0200

    cave and item fixes

---
+++ UnityProject/Assets/Scripts/Actor/Cave/CaveMushrooms.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CaveMushrooms : CommentActorBase {
+
+	override public void OnInspect()
+	{		
+		DisplayComment ("Some strange looking mushrooms.");
+	}
+
+	override public void OnAction()
+	{
+		DisplayComment ("I should probably not touch them.");
+	}
+
+}
