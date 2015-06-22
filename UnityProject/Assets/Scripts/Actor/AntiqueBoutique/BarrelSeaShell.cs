bf60c29172e8c236262e15264243ac9698bec5ab
unknown <nor.aneer@WH93151E.CM.lan>
2015-06-22 13:01:23 +0200

    Added text and polished interior

---
+++ UnityProject/Assets/Scripts/Actor/AntiqueBoutique/BarrelSeaShell.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BarrelSeaShell : InspectBase 
+{
+
+	override public void OnInspect()
+	{		
+		DisplayComment ("It's a barrel full of sea shells.");
+	}
+
+}
