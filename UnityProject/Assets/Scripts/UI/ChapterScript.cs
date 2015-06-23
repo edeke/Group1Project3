b6979e2622b3a7d09dd3713e94dae73d3d3c8267
Naquid <manne.cederborg@yahoo.se>
2015-06-23 14:56:44 +0200

    Added chapter 1 to waterfall level script

---
+++ UnityProject/Assets/Scripts/UI/ChapterScript.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChapterScript : MonoBehaviour {
+
+	float currentTime;
+	float TTL = 5.0f;
+	Animator anim;
+
+
+	// Use this for initialization
+	void Start () 
+	{
+		currentTime = TTL;
+		anim = GetComponent<Animator> ();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		currentTime -= Time.deltaTime;
+
+		if (currentTime <= 1.0f) 
+		{
+			anim.SetBool("Quit",true);
+		}
+
+		if (currentTime <= 0.0f)
+		{
+			Destroy(gameObject);
+		}
+	}
+}
--- UnityProject/Assets/Scripts/World/WaterFallDay.cs
+++ UnityProject/Assets/Scripts/World/WaterFallDay.cs
@@ -1,13 +1,18 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class WaterFallDay : MonoBehaviour {
+public class WaterFallDay : MonoBehaviour 
+{
+
+	string EventID = "Chapter1Seen";
 	
 	public GameObject spawnFromCove;
 	public GameObject spawnFromBeach;
 
 	void Awake()
 	{
+
+		GWorld.TryRegisterEvent (EventID, "Hello");
 
 		switch (GWorld.currentZone) 
 		{
@@ -33,5 +38,29 @@ public class WaterFallDay : MonoBehaviour {
 
 	}
 
+	void Start()
+	{
+		EventData tempData = new EventData();
+		GWorld.FindEvent(EventID,ref tempData);
+			
+		if(!tempData.hasEventOccured)
+		{
+			Invoke ("DisplayChapter1", 1.0f);
+			GWorld.MarkEventDone(EventID);
+		}
 
+	}
+
+	void DisplayChapter1()
+	{
+
+		string path = "Prefabs/UI/Chapters/Chapter1UI";
+		GameObject comp = (GameObject) Instantiate(Resources.Load(path));
+		
+		if(!comp)
+		{
+			Debug.Log ("Failed to Load Letter1 - " + path);
+		}
+	}
+	
 }
