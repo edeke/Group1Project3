7e4c4abc747845418ebe5fe27d388788a2ec2cc3
Naquid <manne.cederborg@yahoo.se>
2015-06-23 16:05:41 +0200

    Update cave

---
+++ UnityProject/Assets/Scripts/Inventory/Items/ItemLetter3.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ItemLetter3 : Item {
+
+	override public void OnInventoryInspect()
+	{
+		//Make sure there is no other letters open
+		UILetter[] letter = FindObjectsOfType<UILetter> ();
+
+		if( letter.Length == 0 )
+		{
+
+			string path = "Prefabs/UI/Letter/Letter3";
+			GameObject letter1 = (GameObject) Instantiate(Resources.Load(path));
+			
+			if(!letter1)
+			{
+				Debug.Log ("Failed to Load Letter3 - " + path);
+			}
+
+		}
+	}
+
+
+	override public void OnInspect()
+	{
+
+		DisplayComment ("A bottle, it looks like it contains a letter.");
+	}
+
+	override public void OnAction( ) 
+	{
+		if (GWorld.isInvEnabled == true) 
+		{
+			if (Inventory.myInv.AddItem (itemData, transform.position)) 
+			{
+				GWorld.MarkEventDone (eventID);
+				Destroy (gameObject);
+			} 
+		} 
+		else 
+		{
+			DisplayComment("Need inventory to pickup item");
+		}
+	}
+
+}
