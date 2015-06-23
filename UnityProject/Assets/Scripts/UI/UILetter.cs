8a9a3e600b1570b7f29d079db890c161b49ea521
Naquid <manne.cederborg@yahoo.se>
2015-06-23 14:07:54 +0200

    Added letter

--- UnityProject/Assets/Scripts/UI/UILetter.cs
+++ UnityProject/Assets/Scripts/UI/UILetter.cs
@@ -4,15 +4,28 @@ using System.Collections;
 public class UILetter : MonoBehaviour 
 {
 
+	Animator anim;
+	
+	void Awake()
+	{
+		anim = GetComponent<Animator> ();
+	}
+
 
 	public void Update()
 	{
 
 
+
 		if (Input.GetMouseButtonUp (0)) 
 		{
-			Destroy (gameObject);
+			anim.SetBool("Quit", true);
+			Invoke ("Destroy", 0.75f);
 		}
 	}
-	
+
+	void Destroy()
+	{
+		Destroy (gameObject);
+	}
 }
