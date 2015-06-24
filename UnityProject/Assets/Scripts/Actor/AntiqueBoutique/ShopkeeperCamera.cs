5248979a59bb182b863020926295c96ce8c09d3d
Naquid <manne.cederborg@yahoo.se>
2015-06-24 11:23:38 +0200

    Finished puzzle in shop Night

---
+++ UnityProject/Assets/Scripts/Actor/AntiqueBoutique/ShopkeeperCamera.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShopkeeperCamera : MonoBehaviour 
+{
+
+	public Camera standardCamera;
+	bool active = false;
+	float activateTime = 0.0f;
+
+	
+
+	// Update is called once per frame
+	void Update () 
+	{
+		if (active) 
+		{
+			activateTime -= Time.deltaTime;
+
+			if(activateTime <= 0.0f)
+			{
+				gameObject.SetActive(false);
+				standardCamera.gameObject.SetActive(true);
+				active = false;
+			}
+
+		}
+	}
+
+
+	public void ActivateSeconds(float time)
+	{
+		activateTime = time;
+		active = true;
+	}
+
+}
--- UnityProject/Assets/Scripts/Inventory/Items/ItemMirror.cs
+++ UnityProject/Assets/Scripts/Inventory/Items/ItemMirror.cs
@@ -6,6 +6,7 @@ public class ItemMirror : Item {
 	int touchCount = 0;
 
 	public GameObject forceLocation;
+	public ShopkeeperCamera camera;
 
 	override public void OnInspect()
 	{
@@ -30,18 +31,26 @@ public class ItemMirror : Item {
 				switch(touchCount)
 				{
 					case 1 :
+						camera.gameObject.SetActive(true);
+						camera.ActivateSeconds(4.0f);
 						FindObjectOfType<Shopkeeper> ().DisplaySpeechBubble("Hey, don't touch that !");
 					break;
 
 					case 2 :
+						camera.gameObject.SetActive(true);
+						camera.ActivateSeconds(4.0f);
 						FindObjectOfType<Shopkeeper> ().DisplaySpeechBubble("I told you to no touch that!");
 					break;
 
 					case 3 :
+						camera.gameObject.SetActive(true);
+						camera.ActivateSeconds(4.0f);
 						FindObjectOfType<Shopkeeper> ().DisplaySpeechBubble("If you touch that one more time...!");
 					break;
 
 					case 4 :
+						camera.gameObject.SetActive(true);
+						camera.ActivateSeconds(4.0f);
 						FindObjectOfType<Shopkeeper> ().DisplaySpeechBubble("That's it, I had it with you. Get out of my shop !!");
 						GWorld.myPlayer.GetComponent<PlayerMovement>().ForceMoveToLocation( forceLocation.transform.position );
 						Invoke ("LoadLevel", 3.0f);
