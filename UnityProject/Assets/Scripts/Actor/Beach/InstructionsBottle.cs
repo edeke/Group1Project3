7ad1ec0b27c918b03412a73e7a0abd3882551743
unknown <william.wennergren@WH932249.CM.lan>
2015-06-25 15:04:00 +0200

    Added instructions bottle.

---
+++ UnityProject/Assets/Scripts/Actor/Beach/InstructionsBottle.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InstructionsBottle : CommentActorBase {
+
+	override public void OnInspect()
+	{		
+		DisplayComment ("A letter. I should read it!");
+	}
+
+	override public void OnAction()
+	{
+//		UILetter[] letter = FindObjectsOfType<UILetter> ();
+//		
+//		if( letter.Length == 0 )
+//		{
+//			
+			string path = "Prefabs/UI/Letter/Letter4";
+			GameObject letter4 = (GameObject) Instantiate(Resources.Load(path));
+			
+//			if(!letter1)
+//			{
+//				Debug.Log ("Failed to Load Letter4 - " + path);
+//			}
+//			
+//		}
+	}
+
+	/*override public void OnTalkTo()
+	{
+
+	}*/
+
+}
