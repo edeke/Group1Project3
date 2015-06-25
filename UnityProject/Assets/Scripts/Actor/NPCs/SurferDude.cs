2af998499c909f7aff60523070e96d492fdfc0e0
Naquid <manne.cederborg@yahoo.se>
2015-06-25 16:56:23 +0200

    hello maaan

--- UnityProject/Assets/Scripts/Actor/NPCs/SurferDude.cs
+++ UnityProject/Assets/Scripts/Actor/NPCs/SurferDude.cs
@@ -3,7 +3,20 @@ using System.Collections;
 
 public class SurferDude : NPCBase 
 {
+	public AudioClip onTalkSound;
 
+	override public void OnTalkTo()
+	{
+		GWorld.PlayClipAt (onTalkSound, transform.position);
+
+		Dialoguer.StartDialogue (dialog, null);
+		currentNPCState = NPCState.talking;
+		
+		if (agent) 
+		{
+			agent.Stop ();
+		}
+	}
 
 	override public void OnInspect()
 	{	
